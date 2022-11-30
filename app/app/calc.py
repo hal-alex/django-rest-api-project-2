8cb054d8427538a5b42d0485918828216ebb41f2
Alex <alex@Alexs-MacBook-Pro.local>
2022-11-30 16:41:04 +0000

    fixed environ issue and added v basic tests

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+def subtract(x,y):
+    """One number minus the other"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_nums(self):
+        """Test adding two numbers"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+    
+    def test_subtract_nums(self):
+        """One number minus another"""
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res, -5)
+

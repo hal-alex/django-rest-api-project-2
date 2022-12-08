5667d296260c76d660993916d08eae666b323f18
Alex <1995aha@gmail.com>
2022-12-08 17:56:56 +0000

    added tag serializer, viewset and registered it in urls, fixed minor test bugs

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -26,8 +26,8 @@ class PublicTagsApiTests(TestCase):
     """Test unauthenticated API requests"""
 
     def setUp(self):
-        self.client = APIClient
-    
+        self.client = APIClient()
+
     def test_auth_required(self):
         """Test auth is required for retrieving tags"""
         res = self.client.get(TAGS_URL)
@@ -40,7 +40,7 @@ class PrivateTagsApiTests(TestCase):
 
     def setUp(self):
         self.user = create_user()
-        self.client = APIClient
+        self.client = APIClient()
         self.client.force_authenticate(self.user)
 
     def test_retrive_tags(self):
@@ -55,11 +55,11 @@ class PrivateTagsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
-    
+
     def test_tags_limited_to_user(self):
         """Test list of tags is limited to authenticated user"""
-        user2 = create_user(email="user@example.com")
-        Tag.objects.create(user2, name="Fruity")
+        user2 = create_user(email="user2@example.com")
+        Tag.objects.create(user=user2, name="Fruity")
         tag = Tag.objects.create(user=self.user, name="Comfort Food")
 
         res = self.client.get(TAGS_URL)
